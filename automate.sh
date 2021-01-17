9d798d6aa1b4a4521231b220d76acfb7116414e2
GithubUser699 <74927253+GithubUser699@users.noreply.github.com>
2021-01-17 04:35:07 +0000

    Update automate.sh
    I use the output of $XDG_TEMPLATES_DIR to get the translated path and check if it already exists or not. If so, I'll just move the contents of the *Templates* folder; if not, I'll first create that folder and than do the former thing.
    As you can see I deleted rsync as it isn't needed any longer.

--- automate.sh
+++ automate.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
-# create the Templates directory in the home folder, if it does not exist
 if [ -f Templates.tar.gz ];
 then
 	# extract the dummy files
-	echo "Extracting.."
+	echo "Extracting files..."
 	tar -xvzf Templates.tar.gz
 
-	# check if ~/Templates already exists
-	if [ ! -d ~/Templates ];
-	then
-		mv Templates ~/Templates
-		echo -e "\nTemplates directory created.."
-	else
-		# directory exists; merge, instad
-		rsync -a Templates/ ~/Templates/
-		rm -rf Templates
+	# if .config/user-dirs.dirs does not exist, create it and append config settings to tell nautilus about our templates and
+	# create the custom Templates directory in the home folder, if it does not exist
+	if [[ ! -f ~/.config/user-dirs.dirs ]];
+		then
+			xdg-user-dirs-update
+			echo "Config file created..."
+		else
+			# this config already exists; append the entry
+			source ~/.config/user-dirs.dirs
+			customPath=$XDG_TEMPLATES_DIR
+			echo "Your Templates path for your language is" $customPath
+				
+				# check if the custom path already exits
+				if [ -d "$customPath" ];
+				then
+					# directory exists; merge
+					echo "Custom path already exists. Merging files..."
+					mv Templates/* $customPath
+					rm -rf Templates
+				else
+					# directory doesn't exit; creating and moving files
+					mkdir $customPath
+					mv Templates/* $customPath
+					rm -rf Templates
+					echo "Created custom Templates folder and added files."
+				fi
 	fi
-
 else
 	echo -e "\nTemplates.tar.gz not found.\nPlease ensure you have the compressed tarball from: https://github.com/angela-d/nautilus-right-click-new-file in the same directory as this script.\nExiting."
 	exit 0
 fi
-echo "Context menu options created.."
 
-# if .config/user-dirs.dirs does not exist, create it and append config settings to tell nautilus about our templates
-if [[ ! -f ~/.config/user-dirs.dirs ]];
-	then
-		xdg-user-dirs-update
-		echo "Config file created.."
-	else
-
-		# this config already exists; append the entry
-		source ~/.config/user-dirs.dirs
-		if [[ $XDG_TEMPLATES_DIR = "$HOME/Templates" ]]
-		then
-			echo "Configuration already exists; skipping.."
-		fi
-
-fi
+echo "Context menu options created..."
 
 # restart nautilus and put it into the background
 nautilus -q && nautilus &
-echo -e "\nInstallation complete!  You should now have context menu options for new files."
+echo -e "\nInstallation complete! You should now have context menu options for new files."
